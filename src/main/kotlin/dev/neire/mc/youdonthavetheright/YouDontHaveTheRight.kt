425ae51246dbbdab6d34f1656d89afdca43f066b
neirenoir <29525767+neirenoir@users.noreply.github.com>
2024-10-07 23:07:21 +0200

    Removed unused imports

--- src/main/kotlin/dev/neire/mc/youdonthavetheright/YouDontHaveTheRight.kt
+++ src/main/kotlin/dev/neire/mc/youdonthavetheright/YouDontHaveTheRight.kt
@@ -5,18 +5,14 @@ import dev.neire.mc.youdonthavetheright.datagen.BrewingRecipesEventListener
 import dev.neire.mc.youdonthavetheright.logic.crafter.CommonLogic
 import dev.neire.mc.youdonthavetheright.recipebook.RecipeBookLogic
 import dev.neire.mc.youdonthavetheright.recipebook.WorldRecipeBook
-import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.config.ModConfig.Type
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.forge.runForDist
 
 /**
  * Main mod class. Should be an `object` declaration annotated with `@Mod`.
